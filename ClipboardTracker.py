bec99591157cd62462e2da3579598ace60c7ff92
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-02-02 12:23:18 +0600

    feat: create clipboeard tracktion

---
+++ ClipboardTracker.py
@@ -0,0 +1,27 @@
+import threading
+import time
+from typing import Callable
+
+import pyperclip
+
+
+class ClipboardTracker(threading.Thread):
+    def __init__(self, callback: Callable[..., None], pause: float = 5.):
+        super(ClipboardTracker, self).__init__()
+        self._callback = callback
+        self._pause = pause
+        self._stopping = False
+
+    def run(self) -> None:
+        recent_value = ""
+        while not self._stopping:
+            tmp_value = pyperclip.paste()
+            if tmp_value != recent_value:
+                recent_value = tmp_value
+
+                self._callback(recent_value)
+
+            time.sleep(self._pause)
+
+    def stop(self) -> None:
+        self._stopping = True
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pynput
 from pynput.keyboard import Key
 from pynput.mouse import Button
 
+from ClipboardTracker import ClipboardTracker
 from configs.hotkeys import double_hotkeys
 
 
@@ -32,9 +33,20 @@ def on_scroll(x: int, y: int, dx: int, dy: int) -> None:
         (x, y)))
 
 
+def save_clipboard_content(clipboard_content: str, path: str = 'clipboard.txt') -> None:
+    with open(path, 'a') as file:
+        file.write(clipboard_content + '\n')
+
+
 def main():
     for shortcut, name in double_hotkeys:
         keyboard.add_hotkey(shortcut, lambda shortcut_name: print(shortcut_name), args=(name,))
+
+    clipboard_tracker = ClipboardTracker(save_clipboard_content)
+    try:
+        clipboard_tracker.start()
+    except KeyboardInterrupt:
+        clipboard_tracker.stop()
 
     mouse_listener = pynput.mouse.Listener(on_click=on_click, on_move=on_move, on_scroll=on_scroll)
     mouse_listener.start()
