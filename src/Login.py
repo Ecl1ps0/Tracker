e95cfbc656bfa582693cca04c5c728eafd5c8ee3
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-04-15 00:31:36 +0500

    feat: add login

---
+++ src/Login.py
@@ -0,0 +1,30 @@
+import requests
+
+
+class Login:
+    def __init__(self):
+        self.is_logged_in = False
+        self.__login_url = "http://localhost:8080/auth/signin"
+
+    def __login(self, login: str, password: str) -> str | None:
+        json_data = {
+            "Email": login,
+            "Password": password
+        }
+
+        response = requests.post(url=self.__login_url, json=json_data)
+        if response:
+            self.is_logged_in = True
+            return response.json()["token"]
+
+        return None
+
+    def start_login(self) -> None:
+        while not self.is_logged_in:
+            email = input("Please write your email: ")
+            password = input("Please write your password: ")
+            token = self.__login(email, password)
+            print(token)
+
+            if not self.is_logged_in:
+                print("Wrong email or password, please try again!")
