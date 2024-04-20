108551c1b7fd68e3ec2953d880d0c62083999a9d
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-04-20 17:49:23 +0500

    chore: refactoring

--- src/loggers/LoggerHandler.py
+++ src/loggers/LoggerHandler.py
@@ -1,15 +1,20 @@
 import logging
 
+from pynput import keyboard, mouse
+
+from ClipboardTracker import ClipboardTracker
+
 
 class LoggerHandler:
-    def __init__(self, trackers: list, logger: logging.Logger):
+    def __init__(self, trackers: list[ClipboardTracker | keyboard.Listener | mouse.Listener], logger: logging.Logger):
         self.trackers = trackers
         self.logger = logger
 
     def start_tracking(self) -> None:
         for tracker in self.trackers:
-            self.logger.info(f"Start {type(tracker).__name__}")
-            tracker.start()
+            if not tracker.is_alive():
+                self.logger.info(f"Start {type(tracker).__name__}")
+                tracker.start()
 
     def stop_tracking(self) -> None:
         for tracker in self.trackers:
