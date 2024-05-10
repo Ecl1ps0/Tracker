279f4d0922f8a81a6836ef219d38a7b51415c30e
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-05-11 01:16:19 +0500

    upd: add logger and message handling

--- src/websocket/WebsocketHandler.py
+++ src/websocket/WebsocketHandler.py
@@ -1,17 +1,30 @@
 import asyncio
+import json
 
 from websockets import WebSocketServerProtocol
+
+from configs.logger import get_logger
+
+logger = get_logger(__name__)
 
 
 class WebsocketHandler:
     def __init__(self):
         self.start_event = asyncio.Event()
         self.end_event = asyncio.Event()
+        self.websocket_message = None
 
     async def handler(self, websocket: WebSocketServerProtocol, path: str) -> None:
         async for message in websocket:
-            print(f"Received from client: {message}")
-            match message:
+            try:
+                msg = json.loads(message)
+                self.websocket_message = msg
+            except json.JSONDecodeError:
+                logger.error("Invalid JSON received")
+                continue
+
+            print(f"Received from client: {msg.get('Message')}")
+            match msg.get('Message'):
                 case "start":
                     self.start_event.set()
                 case "stop":
